ee60e45a4b5eaaf1369bcde4aee5b2719c77eaf1
SHARIFULALAM2025 <sharifullinkdin2025@gmail.com>
2025-07-06 08:02:34 +0600

    javascript math and law

---
+++ index.js
@@ -0,0 +1,77 @@
+// let x = 30;
+// let y = 40;
+// let z = x + y;
+// console.log(z);
+
+// function addition(x ,y) {
+//      z = x + y;
+//      return z;
+// };
+// console.log(addition(20,30));
+
+// function Addition(x,y,z) {
+//      return x + y + z;
+// };
+// console.log(Addition(12,12,12));
+
+
+// function triangleArea(width, height) {
+//      return width * height;
+     
+// };
+// console.log(triangleArea(12.12,24,24));
+// function porisima(width, height) {
+//      return 2 * width * height;
+     
+// }
+// console.log(porisima(6,6));
+// function Adman(x) {
+//      return Math.log(x);
+// }
+// console.log(Adman(1).toFixed(4));
+
+
+// const degree = 30;
+// const rad = degree * Math.PI / 180;
+// function val(x) {
+//      return Math.sin(x)
+// }
+
+// console.log(val(rad));
+
+
+function Degree(deg) {
+     const rox = deg * Math.PI / 180;
+     return Math.sin(rox)
+     
+};
+console.log(Degree(30));
+
+
+const x = 30;
+const y = x * Math.PI / 180;
+const z = Math.sin(y)
+console.log(z);
+
+// triangle area 
+const base = 50;
+const height = 100;
+const area = 1 / 2 * base * height;
+console.log(area);
+
+function Area(base,height) {
+     return 1 / 2 * base * height;
+};
+console.log(Area(50,100));
+// somobahu triangle area
+
+const a = 100;
+const area1 = Math.sqrt (3 )/ 4 * a * a;
+console.log(area1);
+
+
+function Area2(x) {
+     return Math.sqrt(3) / 4 * x * x;
+
+};
+console.log(Area2(100));
